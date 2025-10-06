ce6f542269e81bdcdcd8ac952759ca1678018ebf
Sztorm <marcinwicher99@gmail.com>
2025-10-06 20:59:34 +0200

    Added stringResourceOrElse function and getLocalizedName extension function variants.

--- app/src/main/java/com/sztorm/notecalendar/Extensions.kt
+++ app/src/main/java/com/sztorm/notecalendar/Extensions.kt
@@ -5,13 +5,17 @@ package com.sztorm.notecalendar
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.os.Build
+import androidx.annotation.StringRes
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.res.stringResource
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.Month
 import java.time.YearMonth
 import java.time.format.TextStyle
-import java.util.Locale
+import java.util.Locale as JLocale
 
 val Int.isEven
     get() = (this and 1) == 0
@@ -39,11 +43,202 @@ fun LazyListState.reachedTop(): Boolean {
 val LocalDate.yearMonth: YearMonth
     get() = YearMonth.of(year, month)
 
+@Composable
+@ReadOnlyComposable
+fun stringResourceOrElse(
+    @StringRes id: Int, valueToReplace: String = "?", defaultValue: () -> String
+): String = stringResource(id).let {
+    when (it) {
+        valueToReplace -> defaultValue()
+        else -> it
+    }
+}
+
+fun DayOfWeek.getDefaultLocalizedName() =
+    getDisplayName(TextStyle.FULL_STANDALONE, JLocale.getDefault())
+        .replaceFirstChar { it.uppercaseChar() }
+
+@Composable
+@ReadOnlyComposable
+fun DayOfWeek.getLocalizedName() = when (this) {
+    DayOfWeek.MONDAY -> stringResourceOrElse(R.string.Monday) {
+        getDefaultLocalizedName()
+    }
+
+    DayOfWeek.TUESDAY -> stringResourceOrElse(R.string.Tuesday) {
+        getDefaultLocalizedName()
+    }
+
+    DayOfWeek.WEDNESDAY -> stringResourceOrElse(R.string.Wednesday) {
+        getDefaultLocalizedName()
+    }
+
+    DayOfWeek.THURSDAY -> stringResourceOrElse(R.string.Thursday) {
+        getDefaultLocalizedName()
+    }
+
+    DayOfWeek.FRIDAY -> stringResourceOrElse(R.string.Friday) {
+        getDefaultLocalizedName()
+    }
+
+    DayOfWeek.SATURDAY -> stringResourceOrElse(R.string.Saturday) {
+        getDefaultLocalizedName()
+    }
+
+    DayOfWeek.SUNDAY -> stringResourceOrElse(R.string.Sunday) {
+        getDefaultLocalizedName()
+    }
+}
+
+fun DayOfWeek.getDefaultLocalizedShortName() =
+    getDisplayName(TextStyle.SHORT_STANDALONE, JLocale.getDefault())
+        .replaceFirstChar { it.uppercaseChar() }
+
+@Composable
+@ReadOnlyComposable
+fun DayOfWeek.getLocalizedShortName() = when (this) {
+    DayOfWeek.MONDAY -> stringResourceOrElse(R.string.Monday_Short) {
+        getDefaultLocalizedShortName()
+    }
+
+    DayOfWeek.TUESDAY -> stringResourceOrElse(R.string.Tuesday_Short) {
+        getDefaultLocalizedShortName()
+    }
+
+    DayOfWeek.WEDNESDAY -> stringResourceOrElse(R.string.Wednesday_Short) {
+        getDefaultLocalizedShortName()
+    }
+
+    DayOfWeek.THURSDAY -> stringResourceOrElse(R.string.Thursday_Short) {
+        getDefaultLocalizedShortName()
+    }
+
+    DayOfWeek.FRIDAY -> stringResourceOrElse(R.string.Friday_Short) {
+        getDefaultLocalizedShortName()
+    }
+
+    DayOfWeek.SATURDAY -> stringResourceOrElse(R.string.Saturday_Short) {
+        getDefaultLocalizedShortName()
+    }
+
+    DayOfWeek.SUNDAY -> stringResourceOrElse(R.string.Sunday_Short) {
+        getDefaultLocalizedShortName()
+    }
+}
+
+fun Month.getDefaultLocalizedName() =
+    getDisplayName(TextStyle.FULL_STANDALONE, JLocale.getDefault())
+        .replaceFirstChar { it.uppercaseChar() }
+
+@Composable
+@ReadOnlyComposable
+fun Month.getLocalizedName() = when (this) {
+    Month.JANUARY -> stringResourceOrElse(R.string.January) {
+        getDefaultLocalizedName()
+    }
+
+    Month.FEBRUARY -> stringResourceOrElse(R.string.February) {
+        getDefaultLocalizedName()
+    }
+
+    Month.MARCH -> stringResourceOrElse(R.string.March) {
+        getDefaultLocalizedName()
+    }
+
+    Month.APRIL -> stringResourceOrElse(R.string.April) {
+        getDefaultLocalizedName()
+    }
+
+    Month.MAY -> stringResourceOrElse(R.string.May) {
+        getDefaultLocalizedName()
+    }
+
+    Month.JUNE -> stringResourceOrElse(R.string.June) {
+        getDefaultLocalizedName()
+    }
+
+    Month.JULY -> stringResourceOrElse(R.string.July) {
+        getDefaultLocalizedName()
+    }
+
+    Month.AUGUST -> stringResourceOrElse(R.string.August) {
+        getDefaultLocalizedName()
+    }
+
+    Month.SEPTEMBER -> stringResourceOrElse(R.string.September) {
+        getDefaultLocalizedName()
+    }
+
+    Month.OCTOBER -> stringResourceOrElse(R.string.October) {
+        getDefaultLocalizedName()
+    }
+
+    Month.NOVEMBER -> stringResourceOrElse(R.string.November) {
+        getDefaultLocalizedName()
+    }
+
+    Month.DECEMBER -> stringResourceOrElse(R.string.December) {
+        getDefaultLocalizedName()
+    }
+}
+
+@Composable
+@ReadOnlyComposable
+fun Month.getLocalizedGenitiveCaseName() = when (this) {
+    Month.JANUARY -> stringResourceOrElse(R.string.January_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.FEBRUARY -> stringResourceOrElse(R.string.February_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.MARCH -> stringResourceOrElse(R.string.March_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.APRIL -> stringResourceOrElse(R.string.April_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.MAY -> stringResourceOrElse(R.string.May_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.JUNE -> stringResourceOrElse(R.string.June_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.JULY -> stringResourceOrElse(R.string.July_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.AUGUST -> stringResourceOrElse(R.string.August_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.SEPTEMBER -> stringResourceOrElse(R.string.September_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.OCTOBER -> stringResourceOrElse(R.string.October_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.NOVEMBER -> stringResourceOrElse(R.string.November_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+
+    Month.DECEMBER -> stringResourceOrElse(R.string.December_GenitiveCase) {
+        getDefaultLocalizedName()
+    }
+}
+
 operator fun YearMonth.component1() = year
 
 operator fun YearMonth.component2(): Month = month
 
-fun YearMonth.getDisplayName(style: TextStyle, locale: Locale) =
+fun YearMonth.getDisplayName(style: TextStyle, locale: JLocale) =
     "${month.getDisplayName(style, locale)} $year"
 
 fun YearMonth.getFirstVisibleDay(firstDayOfWeek: DayOfWeek): LocalDate {
@@ -69,6 +264,15 @@ fun YearMonth.getVisibleWeeks(firstDayOfWeek: DayOfWeek): Int {
 
     return days / 7
 }
+
+fun YearMonth.getDefaultLocalizedName() =
+    getDisplayName(TextStyle.FULL_STANDALONE, JLocale.getDefault())
+        .replaceFirstChar { it.uppercaseChar() }
+
+@Composable
+@ReadOnlyComposable
+fun YearMonth.getLocalizedName() =
+    "${month.getLocalizedName()} $year"
 
 /**
  * API level 31 (S) and greater: Checks if [AlarmManager.canScheduleExactAlarms] then calls
--- app/src/main/java/com/sztorm/notecalendar/helpers/DateHelper.kt
+++ app/src/main/java/com/sztorm/notecalendar/helpers/DateHelper.kt
@@ -3,61 +3,17 @@ package com.sztorm.notecalendar.helpers
 import android.content.Context
 import com.sztorm.notecalendar.R
 import java.time.DayOfWeek
-import java.time.Month
 
 class DateHelper {
     companion object {
         fun DayOfWeek.toLocalizedString(context: Context): String = when (this.value) {
-            1 -> context.getString(R.string.lblDayOfWeek_Monday)
-            2 -> context.getString(R.string.lblDayOfWeek_Tuesday)
-            3 -> context.getString(R.string.lblDayOfWeek_Wednesday)
-            4 -> context.getString(R.string.lblDayOfWeek_Thursday)
-            5 -> context.getString(R.string.lblDayOfWeek_Friday)
-            6 -> context.getString(R.string.lblDayOfWeek_Saturday)
-            7 -> context.getString(R.string.lblDayOfWeek_Sunday)
-            else -> "Error"
-        }
-
-        fun DayOfWeek.toLocalizedAbbreviatedString(context: Context): String = when (this.value) {
-            1 -> context.getString(R.string.abbreviation_Monday)
-            2 -> context.getString(R.string.abbreviation_Tuesday)
-            3 -> context.getString(R.string.abbreviation_Wednesday)
-            4 -> context.getString(R.string.abbreviation_Thursday)
-            5 -> context.getString(R.string.abbreviation_Friday)
-            6 -> context.getString(R.string.abbreviation_Saturday)
-            7 -> context.getString(R.string.abbreviation_Sunday)
-            else -> "Error"
-        }
-
-        fun Month.toLocalizedStringGenitiveCase(context: Context): String = when (this.value) {
-            1 -> context.getString(R.string.lblMonth_January)
-            2 -> context.getString(R.string.lblMonth_February)
-            3 -> context.getString(R.string.lblMonth_March)
-            4 -> context.getString(R.string.lblMonth_April)
-            5 -> context.getString(R.string.lblMonth_May)
-            6 -> context.getString(R.string.lblMonth_June)
-            7 -> context.getString(R.string.lblMonth_July)
-            8 -> context.getString(R.string.lblMonth_August)
-            9 -> context.getString(R.string.lblMonth_September)
-            10 -> context.getString(R.string.lblMonth_October)
-            11 -> context.getString(R.string.lblMonth_November)
-            12 -> context.getString(R.string.lblMonth_December)
-            else -> "Error"
-        }
-
-        fun Month.toLocalizedString(context: Context): String = when (this.value) {
-            1 -> context.getString(R.string.January)
-            2 -> context.getString(R.string.February)
-            3 -> context.getString(R.string.March)
-            4 -> context.getString(R.string.April)
-            5 -> context.getString(R.string.May)
-            6 -> context.getString(R.string.June)
-            7 -> context.getString(R.string.July)
-            8 -> context.getString(R.string.August)
-            9 -> context.getString(R.string.September)
-            10 -> context.getString(R.string.October)
-            11 -> context.getString(R.string.November)
-            12 -> context.getString(R.string.December)
+            1 -> context.getString(R.string.Monday)
+            2 -> context.getString(R.string.Tuesday)
+            3 -> context.getString(R.string.Wednesday)
+            4 -> context.getString(R.string.Thursday)
+            5 -> context.getString(R.string.Friday)
+            6 -> context.getString(R.string.Saturday)
+            7 -> context.getString(R.string.Sunday)
             else -> "Error"
         }
     }
