0e31640ff5001f03886585bf15ed397e5870f5d6
Sztorm <marcinwicher99@gmail.com>
2025-10-19 19:37:06 +0200

    Added unfinished ColorPickerPreference component.

---
+++ app/src/main/java/com/sztorm/notecalendar/components/preferences/ColorPickerPreference.kt
@@ -0,0 +1,156 @@
+package com.sztorm.notecalendar.components.preferences
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun ColorPickerPreference(
+    title: String,
+    initialColor: Color,
+    outlineColor: Color,
+    onConfirm: (Color) -> Unit,
+    modifier: Modifier = Modifier,
+    onDismiss: ((Color) -> Unit)? = null,
+    titleColor: Color = Color.Unspecified,
+    summary: String? = null,
+    summaryColor: Color = Color.Unspecified,
+    dialogColors: CardColors = CardDefaults.cardColors(),
+    buttonColor: Color = Color.Unspecified,
+    icon: Painter? = null,
+    iconColorFilter: ColorFilter? = null,
+    enabled: Boolean = true
+) {
+    val titleColor = titleColor.copy(alpha = if (enabled) 1f else 0.4f)
+    val summaryColor = summaryColor.copy(alpha = if (enabled) 0.8f else 0.4f)
+    var openDialog by remember { mutableStateOf(false) }
+    var selectedColor by remember { mutableStateOf(initialColor) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .then(
+                when (enabled) {
+                    true -> Modifier.clickable(onClick = {
+                        selectedColor = initialColor
+                        openDialog = true
+                    })
+
+                    false -> Modifier
+                }
+            )
+            .padding(16.dp)
+    ) {
+        Column(modifier = Modifier.weight(1f)) {
+            Row {
+                Column(Modifier.width(56.dp)) {
+                    if (icon != null) {
+                        Image(
+                            painter = icon,
+                            contentDescription = null,
+                            colorFilter = iconColorFilter
+                        )
+                    }
+                }
+                Column {
+                    Row {
+                        Text(
+                            text = title,
+                            color = titleColor
+                        )
+                    }
+                    if (summary != null) {
+                        Row {
+                            Text(
+                                text = summary,
+                                color = summaryColor,
+                                fontSize = 14.sp,
+                                lineHeight = 16.sp
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            modifier = Modifier
+                .width(64.dp)
+        ) {
+            Box(
+                Modifier
+                    .padding(end = 24.dp)
+                    .drawWithCache {
+                        val stroke = Stroke(width = 2.dp.toPx())
+                        val radius = 16.dp.toPx()
+
+                        onDrawBehind {
+                            drawCircle(
+                                color = initialColor,
+                                radius = radius
+                            )
+                            drawCircle(
+                                color = outlineColor,
+                                radius = radius,
+                                style = stroke
+                            )
+                        }
+                    }
+            )
+        }
+    }
+    if (openDialog) {
+        ConfirmationDialog(
+            onConfirm = {
+                openDialog = false
+                onConfirm(selectedColor)
+            },
+            onDismiss = {
+                openDialog = false
+                selectedColor = initialColor
+                onDismiss?.invoke(selectedColor)
+            },
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true,
+                usePlatformDefaultWidth = false
+            ),
+            dialogColors = dialogColors,
+            textButtonColor = buttonColor
+        ) {
+            Row(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = title,
+                    color = titleColor,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
